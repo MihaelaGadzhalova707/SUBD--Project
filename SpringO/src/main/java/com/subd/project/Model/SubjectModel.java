4d664d009ba786e61235895a4129a5bdab15029d
MihaelaGadzhalova707 <mihaelagadzhalova@gmail.com>
2018-06-15 02:48:49 +0300

    SubjectEntity and SubjectModel

---
+++ SpringO/src/main/java/com/subd/project/Model/SubjectModel.java
@@ -0,0 +1,26 @@
+package com.subd.project.Model;
+
+/**
+ * Created by Misheto on 15.6.2018 г..
+ */
+public class SubjectModel {
+
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

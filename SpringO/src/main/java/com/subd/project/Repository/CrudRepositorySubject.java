60301e7e87a33c5867f191eb9b87aed9dc7e7627
MihaelaGadzhalova707 <mihaelagadzhalova@gmail.com>
2018-06-15 11:54:28 +0300

    findById

--- SpringO/src/main/java/com/subd/project/Repository/CrudRepositorySubject.java
+++ SpringO/src/main/java/com/subd/project/Repository/CrudRepositorySubject.java
@@ -1,5 +1,34 @@
 package com.subd.project.Repository;
 
+
+import com.subd.project.Entities.Student;
+import com.subd.project.Entities.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
 public class CrudRepositorySubject {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    Subject findById(long id) {
+        return jdbcTemplate.queryForObject("select * from student where id=?", new Object[]{id},
+                new SubjectMapper());
+    }
+
+    private static final class SubjectMapper implements RowMapper<Subject> {
+
+        public Subject mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Subject subject = new Subject();
+            subject.setName(rs.getString("name"));
+            return subject;
+        }
+    }
 
 }
